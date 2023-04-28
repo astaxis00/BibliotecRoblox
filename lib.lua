c162faa98fac0d9291590b1b3407a408191ba52d
astaxis00 <130190537+astaxis00@users.noreply.github.com>
2023-04-28 17:56:09 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,58 +1,86 @@
-local GuiLibrary = {}
+local AstLib = {}
 
--- Cria uma nova janela com o título especificado
-function GuiLibrary.new(title)
-    local gui = {}
-    
-     -- Cria a gui
+local Core = Instance.new("ScreenGui")
+Core.Parent = game:GetService("CoreGui")
 
-    local ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Parent = game:GetService("CoreGui")
-    
-    -- Cria a janela
-    local frame = Instance.new("Frame")
-    frame.Size = UDim2.new(0, 200, 0, 200)
-    frame.Position = UDim2.new(0.5, -100, 0.5, -100)
-    frame.BackgroundTransparency = 0.5
-    frame.BackgroundColor3 = Color3.new(0, 0, 0)
-    frame.BorderSizePixel = 2
-    frame.BorderColor3 = Color3.new(1, 1, 1)
-    frame.Active = true
-    frame.Draggable = true
-    frame.Parent = game:GetService("Players").LocalPlayer.PlayerGui
+function AstLib.CreateWindown(name)
+	local Win = Instance.new("Frame")
+	local Header = Instance.new("Frame")
+	local ExecName = Instance.new("TextLabel")
+	
+	Win.Name = "Menu"
+	Win.Parent = Core
+	Win.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
+	Win.BorderSizePixel = 0
+	Win.Position = UDim2.new(0.175014824, 0, 0.430174559, 0)
+	Win.Size = UDim2.new(0, 574, 0, 292)
+	
+	Header.Name = "Header"
+	Header.Parent = Win
+	Header.BackgroundColor3 = Color3.fromRGB(68, 68, 68)
+	Header.BorderSizePixel = 0
+	Header.Size = UDim2.new(0, 574, 0, 33)
 
-    -- Adiciona o título
-    local titleBar = Instance.new("Frame")
-    titleBar.Size = UDim2.new(1, 0, 0, 20)
-    titleBar.BackgroundColor3 = Color3.new(0, 0, 0)
-    titleBar.BorderSizePixel = 0
-    titleBar.Parent = frame
+	ExecName.Name = "ExecName"
+	ExecName.Parent = Header
+	ExecName.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	ExecName.BackgroundTransparency = 1.000
+	ExecName.Position = UDim2.new(0.0087108016, 0, 0, 0)
+	ExecName.Size = UDim2.new(0, 166, 0, 32)
+	ExecName.Font = Enum.Font.GothamBold
+	ExecName.Text = name
+	ExecName.TextColor3 = Color3.fromRGB(255, 255, 255)
+	ExecName.TextScaled = true
+	ExecName.TextSize = 14.000
+	ExecName.TextWrapped = true
+	ExecName.TextXAlignment = Enum.TextXAlignment.Left
+	
+	local function DragScript()
+		local script = Instance.new('LocalScript', Win)
 
-    local titleLabel = Instance.new("TextLabel")
-    titleLabel.Size = UDim2.new(1, -6, 1, -6)
-    titleLabel.Position = UDim2.new(0, 3, 0, 3)
-    titleLabel.BackgroundTransparency = 1
-    titleLabel.Text = title
-    titleLabel.TextColor3 = Color3.new(1, 1, 1)
-    titleLabel.TextSize = 16
-    titleLabel.Font = Enum.Font.SourceSansBold
-    titleLabel.TextXAlignment = Enum.TextXAlignment.Left
-    titleLabel.Parent = titleBar
+		local UserInputService = game:GetService("UserInputService")
 
-    -- Adiciona um botão
-    function gui.button(text, callback)
-        local button = Instance.new("TextButton")
-        button.Text = text
-        button.Size = UDim2.new(0, 100, 0, 30)
-        button.Position = UDim2.new(0.5, -50, 0.5, 30)
-        button.Parent = frame
+		local gui = Win
 
-        button.MouseButton1Click:Connect(function()
-            callback()
-        end)
-    end
+		local dragging
+		local dragInput
+		local dragStart
+		local startPos
 
-    return gui
+		local function update(input)
+			local delta = input.Position - dragStart
+			gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		end
+
+		gui.InputBegan:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				dragging = true
+				dragStart = input.Position
+				startPos = gui.Position
+
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragging = false
+					end
+				end)
+			end
+		end)
+
+		gui.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+
+		UserInputService.InputChanged:Connect(function(input)
+			if input == dragInput and dragging then
+				update(input)
+			end
+		end)
+
+	end
+	coroutine.wrap(DragScript)()
+
 end
 
-return GuiLibrary
+return AstLib
